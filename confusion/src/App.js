bdca96cef72c0a7adff0a63d0f58570d9f061ff8
1badnews <aras.butrimanskas@gmail.com>
2022-09-19 21:54:08 +0300

    dziugui fix pls

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -2,8 +2,22 @@ import logo from './logo.svg';
 import  {Navbar, NavbarBrand}  from 'reactstrap';
 import './App.css';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
 function App() {
+/* sita reikia pakeisti
+
+
+  constructor(props){
+    super(props);
+
+    this.state={
+      dishes:DISHES
+    };
+  }
+
+
+*/
   return (
     <div>
       <Navbar dark color="primary">
@@ -11,9 +25,12 @@ function App() {
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu/>
+      <Menu dishes={this.state.dishes} />   
     </div>
   );
 }
+/*
+<Menu dishes={this.state.dishes} /> (line 28) irgi nezinau ar yra gerai
+*/
 
 export default App;
