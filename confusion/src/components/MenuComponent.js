96fb0929cede7b501045aa565dbfbb29bba1d045
1badnews <aras.butrimanskas@gmail.com>
2022-09-29 16:58:57 +0300

    assignment1 complete

--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -27,59 +27,7 @@ class Menu extends Component {
         console.log(dish)
     }
 
-    renderDish(dish) {
-        if (dish != null) {
-            return(
-                
-
-                
-                <div className="col-md-5 col-sm-12 m-1">
-                    <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                </div>
-                
-            
-                
-            );
-        }
-        else {
-            return(
-                <div></div>
-            )
-        }
-    }
-    renderComments(dish) {
-        if (dish != null) {
-            return(
-                
-            <div className="col-md-5 col-sm-12 m-1">
-                <h3> Comments </h3>
-                <p>{dish.comments[0].comment}</p>
-                <p>-- {dish.comments[0].author} , {dish.comments[0].date}</p>
-                <p>{dish.comments[1].comment}</p>
-                <p>-- {dish.comments[1].author} , {dish.comments[1].date}</p>
-                <p>{dish.comments[2].comment}</p>
-                <p>-- {dish.comments[2].author} , {dish.comments[2].date}</p>
-                <p>{dish.comments[3].comment}</p>
-                <p>-- {dish.comments[3].author} , {dish.comments[3].date}</p>
-                <p>{dish.comments[4].comment}</p>
-                <p>-- {dish.comments[4].author} , {dish.comments[4].date}</p>
-            </div>
-            
-                
-            );
-        }
-        else {
-            return(
-                <div></div>
-            )
-        }
-    }
+   
 
     render(){
 
